0ce3836e7a9283800056c5e9753d6ab0e4d3c833
AmarKakde <amarkakde21@gmail.com>
2024-04-01 12:19:47 +0530

    greet a person based on gender

--- week-1-1.2.js
+++ week-1-1.2.js
@@ -11,30 +11,4 @@ function main(){
     console.log(greeting(first_name, last_name));
 }
 
-//main();
-
-//Write a program to greet a person based on their gender {if else}
-
-function greeting_on_gender(first_name, last_name, gender){
-    if(gender=='male'){
-        return `Hello, Mr.${first_name} ${last_name}`;
-    }
-    else if(gender=='female')
-    {
-        return `Hello, Ms.${first_name} ${last_name}`;
-    }
-    else
-    {
-        return `invalid gender`;
-    }
-}
-
-function main2(){
-    const first_name = 'Akash';
-    const last_name = 'Singh';
-    const gender = 'female'
-
-    console.log(greeting_on_gender(first_name, last_name, gender));
-}
-
-main2();
+main();
