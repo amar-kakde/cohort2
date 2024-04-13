530d875d96619875bc57e755d9b4a794f302b5c3
amar-kakde <amar.kakde@outlook.com>
2024-04-13 12:22:38 +0530

    interest calculate

--- DOM/backend_server.js
+++ DOM/backend_server.js
@@ -14,6 +14,20 @@ app.get("/calculatesum", function (req, res) {
 	res.status(200).send(sum.toString());
 });
 
+app.get("/interest", function (req, res) {
+	const principal = parseFloat(req.query.principal);
+	const interest = parseFloat(req.query.interest);
+	const tenure = parseFloat(req.query.tenure);
+
+	let simpleInterest = (principal * interest * tenure) / 100;
+	const TotalAmount = principal + simpleInterest;
+
+	res.status(200).json({
+		simpleInterest: simpleInterest,
+		TotalAmount: TotalAmount,
+	});
+});
+
 app.listen(port, function () {
 	console.log(`server is listening on ${port}`);
 });
