c41f4fd5b1bbfc1a868aae51d05a73db43a7f681
amar-kakde <amar.kakde@outlook.com>
2024-04-20 23:44:24 +0530

    backend create todo and update todo

--- todoapp/backend/index.js
+++ todoapp/backend/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { createTodo, updateTodo } = require("./types.js");
 const port = 3000;
 const app = express();
 
@@ -9,11 +10,27 @@ app.get("/todos", function (req, res) {
 });
 
 app.post("/todo", function (req, res) {
-	//
+	const createPayload = req.body;
+	const parsedPayload = createTodo.safeParse(createPayload);
+	if (!parsedPayload.success) {
+		res.status(401).json({
+			msg: "you sent the wrong inputs",
+		});
+		return;
+	}
+	// put in mongo db
 });
 
 app.post("/completed", function (req, res) {
-	//
+	const updatePayload = req.body;
+	const parsedPayload = updateTodo.safeParse(updatePayload);
+	if (!parsedPayload.success) {
+		res.status(401).json({
+			msg: "you sent the wrong input",
+		});
+		return;
+	}
+	// update in mongo db
 });
 
 app.listen(port, function () {
